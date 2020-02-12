67e69bbbcd34b5eaa76166213356a74319cfa310
Julien Boubechtoula <julien.boubechtoula@gmail.com>
2020-02-12 23:24:45 +0100

    Update catalog entry templates

--- src/main/java/org/kathra/appmanager/catalogentry/CatalogEntryTemplates.java
+++ src/main/java/org/kathra/appmanager/catalogentry/CatalogEntryTemplates.java
@@ -10,19 +10,21 @@ public class CatalogEntryTemplates {
 
     public List<CatalogEntryTemplate> getTemplates() {
         org.kathra.appmanager.model.CatalogEntryTemplate restApiFromImplementation =  new org.kathra.appmanager.model.CatalogEntryTemplate().name("RestApiFromImplementation")
-                .addArgumentsItem(new CatalogEntryTemplateArgument().key("NAME"))
-                .addArgumentsItem(new CatalogEntryTemplateArgument().key("DESCRIPTION"))
-                .addArgumentsItem(new CatalogEntryTemplateArgument().key("GROUP_PATH"))
-                .addArgumentsItem(new CatalogEntryTemplateArgument().key("IMPLEMENTATION_ID"))
-                .addArgumentsItem(new CatalogEntryTemplateArgument().key("IMPLEMENTATION_VERSION"));
+                .label("Rest API from Implementation")
+                .addArgumentsItem(new CatalogEntryTemplateArgument().key("NAME").label("Catalog entry name"))
+                .addArgumentsItem(new CatalogEntryTemplateArgument().key("DESCRIPTION").label("Description owner"))
+                .addArgumentsItem(new CatalogEntryTemplateArgument().key("GROUP_PATH").label("Group owner"))
+                .addArgumentsItem(new CatalogEntryTemplateArgument().key("IMPLEMENTATION_ID").label("Implementation"))
+                .addArgumentsItem(new CatalogEntryTemplateArgument().key("IMPLEMENTATION_VERSION").label("Implementation version"));
 
         org.kathra.appmanager.model.CatalogEntryTemplate restApiFromImage =  new org.kathra.appmanager.model.CatalogEntryTemplate().name("RestApiFromDockerImage")
-                .addArgumentsItem(new CatalogEntryTemplateArgument().key("NAME"))
-                .addArgumentsItem(new CatalogEntryTemplateArgument().key("DESCRIPTION"))
-                .addArgumentsItem(new CatalogEntryTemplateArgument().key("GROUP_PATH"))
-                .addArgumentsItem(new CatalogEntryTemplateArgument().key("IMAGE_NAME"))
-                .addArgumentsItem(new CatalogEntryTemplateArgument().key("IMAGE_TAG"))
-                .addArgumentsItem(new CatalogEntryTemplateArgument().key("IMAGE_REGISTRY"));
+                .label("Rest API from Docker Image")
+                .addArgumentsItem(new CatalogEntryTemplateArgument().key("NAME").label("Catalog entry name"))
+                .addArgumentsItem(new CatalogEntryTemplateArgument().key("DESCRIPTION").label("Description"))
+                .addArgumentsItem(new CatalogEntryTemplateArgument().key("GROUP_PATH").label("Group owner"))
+                .addArgumentsItem(new CatalogEntryTemplateArgument().key("IMAGE_NAME").label("Docker image name"))
+                .addArgumentsItem(new CatalogEntryTemplateArgument().key("IMAGE_TAG").label("Docker image tag"))
+                .addArgumentsItem(new CatalogEntryTemplateArgument().key("IMAGE_REGISTRY").label("Docker registry host"));
 
         return ImmutableList.of(restApiFromImplementation, restApiFromImage);
     }
